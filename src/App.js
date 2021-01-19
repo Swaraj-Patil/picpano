24cfe629041edc1b3afca8cc9ca50e28989d1fb9
Swaraj Patil <68634770+Swaraj-Patil@users.noreply.github.com>
2021-01-19 21:20:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,21 +5,17 @@ import MainPage from './components/pages/MainPage'
 import Images from './components/pages/Images'
 import Posts from './components/pages/Posts'
 import Albums from './components/pages/Albums'
-import PageNotFound from './components/pages/404'
+//import PageNotFound from './components/pages/404'
 
 
 function App() {
   
   return (
     <div className="App">
-        <Switch>
-          <Route exact path="/" component={MainPage} />
-          <Route exact path="/images" component={Images} />
-          <Route exact path="/Posts" component={Posts} />
-          <Route exact path="/Albums" component={Albums} />
-          <Route exact path="/404" component={PageNotFound} />
-          <Redirect to="/404" />
-        </Switch>
+      <Route exact path="/" component={MainPage} />
+      <Route path="/images" component={Images} />
+      <Route path="/Posts" component={Posts} />
+      <Route path="/Albums" component={Albums} />
     </div>
   );
 }
